b7f09d0f4c8e7aec021671808155b359df929465
panupong rong-o <tayalone@gmail.com>
2022-11-22 09:21:31 +0700

    :sparkles: init gracegully shutting down gin

---
+++ gracefully-shuttingdown/gin/go.mod
@@ -0,0 +1,3 @@
+module github.com/tayalone/go-example/racefully-shuttingdown/gin
+
+go 1.19
---
+++ gracefully-shuttingdown/gin/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("i will do grace fully shutting donw")
+}
